dd2f0b189fd69b3bba70acfeacdad399a6de07cd
dionce <dionisenja@gmail.com>
2021-06-16 10:56:06 +0200

    Added Maps

--- seeds/index.js
+++ seeds/index.js
@@ -30,6 +30,10 @@ const seedDB = async () => {
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!",
       price,
+      geometry: {
+        type: "Point",
+        coordinates: [-113.1331, 47.0202],
+      },
       images: [
         {
           url: "https://res.cloudinary.com/dbemcqaxd/image/upload/v1623751927/YelpCamp/pfpf5wki03ggqz6nafvs.jpg",
